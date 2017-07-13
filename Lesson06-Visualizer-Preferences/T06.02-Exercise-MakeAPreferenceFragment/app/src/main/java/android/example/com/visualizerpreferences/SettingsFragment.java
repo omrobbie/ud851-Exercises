dfef3fe8ab32181fdd2a101a8d9a05e4c10d6daa
omrobbie <putu.roby@yahoo.com>
2017-07-13 10:56:50 +0800

    T06.02 L2 11. Exercise: Make a Preference Fragment

---
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -0,0 +1,26 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * Created by omrobbie on 13/07/2017.
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    /**
+     * Called during {@link #onCreate(Bundle)} to supply the preferences for this fragment.
+     * Subclasses are expected to call {@link #setPreferenceScreen(PreferenceScreen)} either
+     * directly or via helper methods such as {@link #addPreferencesFromResource(int)}.
+     *
+     * @param savedInstanceState If the fragment is being re-created from
+     *                           a previous saved state, this is the state.
+     * @param rootKey            If non-null, this preference fragment should be rooted at the
+     *                           {@link PreferenceScreen} with this key.
+     */
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_visualizer);
+    }
+}
